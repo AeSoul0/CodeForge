0df52e669895277f83a08fee90e35e8a6fc2c7cd
ÆSoul <samuelearabia10@gmail.com>
2026-08-17 17:37:39 +0200

    fix(frontend): enable Vercel SSR

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+
   integrations: [react()],
 
   redirects: {
@@ -13,10 +17,11 @@ export default defineConfig({
   },
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
+
   server: {
     port: 2003,
     host: true,
-  }
+  },
 });
